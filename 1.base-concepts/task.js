22079eea03ed19eccc0777990489e9a2a7dc6d95
Pavel Efimov <gform7@gmail.com>
2022-08-14 13:44:23 +0300

    Task 1 Realization

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,22 @@
+"use strict";
+ 
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = Math.pow(b, 2) - (4 * a * c);
+  
+  if (d < 0) {
+    arr.push();
+ 
+  } else if (d == 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+  
+  } else if (d > 0) {
+    let firstRoot = (-b + Math.sqrt(d)) / (2 * a);
+    let secondRoot = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(firstRoot, secondRoot);
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
