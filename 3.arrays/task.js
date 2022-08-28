e2f953c52f6083e9208dd46144288b6d2931757e
Pavel Efimov <gform7@gmail.com>
2022-08-28 18:56:49 +0300

    Tasks 1-2 realization

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = (arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]));
+  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter((positive) => positive > 0)
+    .filter ((multipe) => multipe % 3 === 0)
+    .map ((multiplied) => multiplied * 10)
+  return resultArr;
 }
