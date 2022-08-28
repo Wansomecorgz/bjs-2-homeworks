99358d1d35b227c6fff8d532fe4847a0679cadbc
Pavel Efimov <gform7@gmail.com>
2022-08-28 21:43:01 +0300

    Task realization

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,30 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark){
+  if (this.marks === undefined) this.marks = []; 
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...mark){
+  if (this.marks === undefined) this.marks = [];
+  for (element of mark) this.marks.push(element);
+}
+
+Student.prototype.getAverage = function (){
+  let reducer = (previousValue, currentValue) => previousValue + currentValue;
+  return (this.marks.reduce(reducer) / this.marks.length);
+}
+
+Student.prototype.exclude = function (reason){
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
