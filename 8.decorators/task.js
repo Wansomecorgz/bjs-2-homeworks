7a3b7de140266de6e3f6d294b63847ab0b6b1817
Pavel Efimov <gform7@gmail.com>
2022-09-19 15:31:24 +0300

    Tasks 1-2 realization

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,35 @@
+//Task 1
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    let hash = args.join(',');
+    if (hash in cache) {
+      return "Из кэша: " + cache[hash];
+    } else {
+      cache[hash] = func.apply(this, args);
+      if (Object.keys(cache).length > 4) delete cache[Object.keys(cache)[0]];
+      return "Вычисляем: " + cache[hash];
+    }
+  }
+  return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+//Task 2
+function debounceDecoratorNew(func, delay) {
+  let timeoutId;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  function wrapper(...args) {
+     if (!timeoutId) {
+        wrapper.count++;
+        func(...args);
+     }
+     wrapper.allCount++;
+     clearTimeout(timeoutId);
+     timeoutId = setTimeout(() => {
+        wrapper.count++;
+        return func(...args);
+     }, delay)
+  }
+  return wrapper;
 }
